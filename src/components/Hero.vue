f22ead772a27a29b913c0e6a1eea39f65e27c64a
alifma <alifmaulana26@gmail.com>
2020-12-27 10:55:58 +0700

    Hero Mobile & Desktop

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,21 +1,49 @@
 <template>
-    <div class="row mt-4">
-        <div class="col">
-            <div class="d-flex h-100">
-                <div class="justify-content-center align-self-center">
-                    <h2><strong>Delicious Food Menu,</strong>
-                        <br> in your gadget</h2>
-                    <p>Ayo segera pilih dan pesan makanan favorit anda!</p>
-                    <button class="btn btn-lg btn-success">
-                        <b-icon-arrow-right></b-icon-arrow-right> Pesan
-                    </button>
+    <div class="hero">
+        <!-- Desktop -->
+        <div class="d-none d-md-block">
+            <div class="row mt-4">
+                <div class="col-md-6">
+                    <div class="d-flex h-100">
+                        <div class="justify-content-center align-self-center">
+                            <h2><strong>Delicious Food Menu,</strong>
+                                <br> in your gadget</h2>
+                            <p>Ayo segera pilih dan pesan makanan favorit anda!</p>
+                            <button class="btn btn-lg btn-success">
+                                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="../assets/img/hero.png" width="100%" alt="Test">
                 </div>
             </div>
         </div>
-        <div class="col">
-            <img src="../assets/img/hero.png" width="100%" alt="Test">
+        <!-- Mobile -->
+        <div class="d-sm-block d-md-none">
+            <div class="row mt-4">
+                <div class="col-md-6 mb-2">
+                    <img src="../assets/img/hero.png" width="100%" alt="Test">
+                </div>
+                <div class="col-md-6">
+                    <div class="d-flex h-100">
+                        <div class="justify-content-center align-self-center">
+                            <h2><strong>Delicious Food Menu,</strong>
+                                <br> in your gadget</h2>
+                            <p>Ayo segera pilih dan pesan makanan favorit anda!</p>
+                            <button class="btn btn-lg btn-success">
+                                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
+
     </div>
+
 </template>
 
 <script>
