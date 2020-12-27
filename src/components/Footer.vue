75bc93dbde2e76880191b9b235d6412787a47856
alifma <alifmaulana26@gmail.com>
2020-12-27 12:13:19 +0700

    Axios Connection + Product Card

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="my-5">
+        <hr>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    &copy; 2020 AlifmaDev
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: 'Footer'
+    }
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,21 @@
     <Navbar />
     <div class="container">
       <Hero />
+      <div class="row mt-4">
+        <div class="col">
+          <h1>Best<strong>Foods</strong></h1>
+        </div>
+        <div class="">
+          <router-link class="btn btn-success float-right" to="/foods">
+            <b-icon-eye></b-icon-eye> Lihat Semua
+          </router-link>
+        </div>
+      </div>
+      <div class="row mb-4">
+        <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
+          <CardProduct :product="product" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,12 +26,38 @@
   // @ is an alias to /src
   import Navbar from '@/components/Navbar.vue'
   import Hero from '@/components/Hero.vue'
+  import CardProduct from '@/components/CardProduct.vue'
+  import axios from 'axios'
 
   export default {
     name: 'Home',
     components: {
       Navbar,
-      Hero
+      Hero,
+      CardProduct
+    },
+    data() {
+      return {
+        products: []
+      }
+    },
+    methods: {
+      setProduct(data) {
+        this.products = data
+      }
+    },
+    mounted() {
+      // Make a request for a user with a given ID
+      axios.get('http://localhost:3000/best-products')
+        .then(response =>
+          // handle success
+          this.setProduct(response.data)
+        )
+        .catch(error =>
+          // handle error
+          console.log("Gagal : ", error)
+        )
+
     }
   }
 </script>
